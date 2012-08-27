c1fa09d7fd8506b4f01722358b8cf816106a59fb
Jean-Philippe Encausse <Jp@encausse.net>
2012-08-27 23:43:09 +0200

    First Introduce WSRSpeak code

---
+++ WSRSpeak/SayTTS.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Speech.Synthesis;
+
+namespace encausse.net {
+    class SayTTS {
+
+        // -----------------------------------------
+        //  MAIN
+        // -----------------------------------------
+
+        static void Main(string[] args) {
+            if (args.Length != 1) {
+                Console.WriteLine("Usage: SayTTS.exe \"text to speech\"");
+                return;
+            }
+            say(args[0]);
+        }
+
+        public static void say(String tts) {
+            Console.WriteLine("[TTS] Say: {0}", tts);
+            using (SpeechSynthesizer synthesizer = new SpeechSynthesizer()) {
+
+                // Configure the audio output.
+                synthesizer.SetOutputToDefaultAudioDevice();
+
+                // Build and speak a prompt.
+                PromptBuilder builder = new PromptBuilder();
+                builder.AppendText(tts);
+                synthesizer.Speak(builder);
+            }
+        }
+    }
+}
